c55cb5f31490d55319a63546f3ecde8e756b82ef
TheSola10 <me@thesola.io>
2024-10-09 16:26:31 +0200

    initial commit

---
+++ ostree-sysext/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# My Awesome Script (c) Karim Vergnes <me@thesola.io>
+# Does something awesome
+# Licensed for private use exclusively
+
+"""
+Docstring
+"""
+
+if __name__ == '__main__':
+    pass
